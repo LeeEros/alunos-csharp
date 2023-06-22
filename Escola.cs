9ab1f9988be13616b2963a1d12d1eee21a8a6752
LeeEros <95001466+LeeEros@users.noreply.github.com>
2023-06-21 22:18:45 -0300

    Feat: Program

--- Escola.cs
+++ Escola.cs
@@ -5,8 +5,8 @@ using Newtonsoft.Json;
 
 
 namespace Escola{
-    public class Escola{
-        public List<Aluno> Alunos = new List<Aluno>();
+	public class Escola{
+		public List<Aluno> Alunos = new List<Aluno>();
 		
 		public List<Professor> Professores = new List<Professor>();
 
@@ -14,13 +14,35 @@ namespace Escola{
 
 		public List<Materia> Materias = new List<Materia>();
 
-        public void SalvarDados(){
-            File.WriteAllText("Alunos.json", JsonConvert.SerializeObject(Alunos));
-            File.WriteAllText("Professores.json", JsonConvert.SerializeObject(Professores));
-            File.WriteAllText("Turmas.json", JsonConvert.SerializeObject(Turmas));
-            File.WriteAllText("Materias.json", JsonConvert.SerializeObject(Materias));
-       }
-
-
-    }
+		public void SalvarDados(){
+			File.WriteAllText("Alunos.json", JsonConvert.SerializeObject(Alunos));
+			File.WriteAllText("Professores.json", JsonConvert.SerializeObject(Professores));
+			File.WriteAllText("Turmas.json", JsonConvert.SerializeObject(Turmas));
+			File.WriteAllText("Materias.json", JsonConvert.SerializeObject(Materias));
+	   }
+	   
+	   public void CarregarDados()
+	   {
+			if(File.Exists("Alunos.json"))
+			{
+				Alunos = JsonConvert.DeserializeObject<List<Aluno>>(File.ReadAllText("Alunos.json"));
+			}
+			
+			if(File.Exists("Professores.json"))
+			{
+				Professores = JsonConvert.DeserializeObject<List<Professor>>(File.ReadAllText("Professores.json"));
+			}
+			
+			if(File.Exists("Turmas.json"))
+			{
+				Turmas = JsonConvert.DeserializeObject<List<Turma>>(File.ReadAllText("Turmas.json"));
+			}
+			
+			if(File.Exists("Materias.json"))
+			{
+				Materias = JsonConvert.DeserializeObject<List<Materia>>(File.ReadAllText("Materias.json"));
+			}
+			
+	   }
+	}
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,49 @@
 namespace Escola{
 	class Program{
 		static void Main(string[] args){
-			Console.WriteLine("-- BEM VINDO AO SISTEMA --");
+			Escola escola = new Escola();
+			escola.CarregarDados();
+			
+			int opcao;
+			
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Bem-vindo ao sistema");
+				Console.WriteLine("Selecione uma opção:");
+				Console.WriteLine("1 - Cadastro");
+				Console.WriteLine("2 - Consultas");
+				Console.WriteLine("3 - Empréstimos");
+				Console.WriteLine("4 - Devoluções");
+				Console.WriteLine("0 - Sair");
+
+				int.TryParse(Console.ReadLine(), out opcao);
+
+				switch (opcao)
+				{
+					case 1:
+						Cadastro(biblioteca);
+						break;
+					case 2:
+						Consultas(biblioteca);
+						break;
+					case 3:
+						Emprestimos(biblioteca);
+						break;
+					case 4:
+						Devolucoes(biblioteca);
+						break;
+					case 0:
+						Console.WriteLine("Encerrando o sistema...");
+						biblioteca.SalvarDados();
+						break;
+					default:
+						Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+						Console.ReadKey();
+						break;
+				}
+			} while (opcao != 0);
+			
 		}
 	}
 }
