72fdf0616efaf31ff143bb31157e56b41038f4f0
Gian Ilchechen <giancilchechen@gmail.com>
2023-07-05 00:23:27 -0300

    feat: método de exibir informações do aluno

--- Aluno.cs
+++ Aluno.cs
@@ -6,5 +6,15 @@ namespace Escola
     {
         public int MatriculaAluno { get; set; }
         public string EmailAluno { get; set; }
+
+        public override void ExibirInformacoes(){
+            int idade = calculaIdade();
+
+            Console.WriteLine("Nome do Aluno: " + Nome);
+            Console.WriteLine("Data de Nascimento: " + DataNascimento + " - " + idade + " anos");
+            Console.WriteLine("Email: " + EmailAluno);
+            Console.WriteLine("Matricula: " + MatriculaAluno);
+            
+        }
     }
 }
