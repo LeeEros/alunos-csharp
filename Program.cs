ccf4f3e887518faadef59b6c43feb22d7581e662
Gian Ilchechen <giancilchechen@gmail.com>
2023-07-05 00:31:39 -0300

    refactor: ajustes de menu

--- Program.cs
+++ Program.cs
@@ -1,134 +1,148 @@
 ﻿using System;
 
-namespace Escola
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Escola escola = new Escola();
-            escola.CarregarDados();
+namespace Escola {
+   class Program {
+      static void Main(string[] args) {
+         Escola escola = new Escola();
+         escola.CarregarDados();
 
-            int opcao;
+         int opcao;
 
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("Bem-vindo ao sistema");
-                Console.WriteLine("Selecione uma opção:");
-                Console.WriteLine("1 - Cadastro");
-                Console.WriteLine("2 - Consultas");
-                Console.WriteLine("3 - Alterações");
-                Console.WriteLine("0 - Sair");
-
-                int.TryParse(Console.ReadLine(), out opcao);
-
-                switch (opcao)
-                {
-                    case 1:
-                        Cadastros(escola);
-                        break;
-                    case 2:
-                        Consultas(escola);
-                        break;
-                    case 3:
-                        Alteracoes(escola);
-                        break;
-                    case 0:
-                        Console.WriteLine("Encerrando o sistema...");
-                        escola.SalvarDados();
-                        break;
-                    default:
-                        Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
-                        Console.ReadKey();
-                        break;
-                }
-            } while (opcao != 0);
-        }
-
-        static void Cadastros(Escola escola)
-        {
+         do {
             Console.Clear();
-            Console.WriteLine("Selecione uma opção de cadastro:");
-            Console.WriteLine("1 - Cadastrar Aluno");
-            Console.WriteLine("2 - Cadastrar Professor");
-            Console.WriteLine("3 - Cadastrar Matéria");
-            Console.WriteLine("4 - Cadastrar Turma");
-            Console.WriteLine("5 - Vincular Aluno a turma");
-            Console.WriteLine("0 - Voltar");
+            Console.WriteLine("Bem-vindo ao sistema");
+            Console.WriteLine("Selecione uma opção:");
+            Console.WriteLine("1 - Cadastro");
+            Console.WriteLine("2 - Consultas");
+            Console.WriteLine("3 - Alterações");
+            Console.WriteLine("0 - Sair");
 
-            int.TryParse(Console.ReadLine(), out int opcao);
+            int.TryParse(Console.ReadLine(), out opcao);
 
-            switch (opcao)
-            {
-                case 1:
-                    escola.CadastrarAluno(escola);
-                    break;
-
-                case 2:
-                    escola.CadastrarProfessor(escola);
-                    break;
-
-                case 3:
-                    escola.CadastrarMateria(escola);
-                    break;
-
-                case 4:
-                    escola.CadastrarTurmas(escola);
-                    break;
-
-                case 5:
-                    escola.VincularAlunoATurma(escola);
-                    break;
-
-                case 0:
-                    Console.WriteLine("Voltando...");
-                    Console.ReadKey();
-                    break;
+            switch (opcao) {
+            case 1:
+               Cadastros(escola);
+               break;
+            case 2:
+               Consultas(escola);
+               break;
+            case 3:
+               Alteracoes(escola);
+               break;
+            case 0:
+               Console.WriteLine("Encerrando o sistema...");
+               break;
+            default:
+               Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+               Console.ReadKey();
+               break;
             }
-        }
+         } while (opcao != 0);
+      }
 
-        static void Consultas(Escola escola){
-			Console.Clear();
-            Console.WriteLine("Selecione uma opção de cadastro:");
-            Console.WriteLine("1 - Consultar Aluno");
-            Console.WriteLine("2 - Consultar Professor");
-            Console.WriteLine("3 - Consultar Matéria");
-            Console.WriteLine("4 - Consultar Turma");
-            Console.WriteLine("0 - Voltar");
+      static void Cadastros(Escola escola) {
+         Console.Clear();
+         Console.WriteLine("Selecione uma opção de cadastro:");
+         Console.WriteLine("1 - Cadastrar Aluno");
+         Console.WriteLine("2 - Cadastrar Professor");
+         Console.WriteLine("3 - Cadastrar Matéria");
+         Console.WriteLine("4 - Cadastrar Turma");
+         Console.WriteLine("0 - Voltar");
 
-            int.TryParse(Console.ReadLine(), out int opcao);
+         int.TryParse(Console.ReadLine(), out int opcao);
 
-            switch (opcao)
-            {
-                case 1:
-                escola.ConsultaAluno(escola);				   
-                break;
+         switch (opcao) {
+         case 1:
+            escola.CadastrarAluno(escola);
+            break;
 
-                case 2:
-                    
-                break;
+         case 2:
+            escola.CadastrarProfessor(escola);
+            break;
 
-                case 3:
-                    
-                break;
+         case 3:
+            escola.CadastrarMateria(escola);
+            break;
 
-                case 4:
-                   
-                break;
+         case 4:
+            escola.CadastrarTurmas(escola);
+            break;
 
-                case 0:
-                    Console.WriteLine("Voltando...");
-                    Console.ReadKey();
-                break;
-            }
-			
+         case 0:
+            Console.WriteLine("Voltando...");
+            break;
 
-        }
+         default:
+            Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            break;
+         }
+      }
 
-        static void Alteracoes(Escola escola)
-        {
+      static void Consultas(Escola escola) {
+         Console.Clear();
+         Console.WriteLine("Selecione uma opção de cadastro:");
+         Console.WriteLine("1 - Consultar Aluno");
+         Console.WriteLine("2 - Consultar Professor");
+         Console.WriteLine("3 - Consultar Matéria");
+         Console.WriteLine("4 - Consultar Turma");
+         Console.WriteLine("0 - Voltar");
 
-        }
-    }
+         int.TryParse(Console.ReadLine(), out int opcao);
+
+         switch (opcao) {
+         case 1:
+            escola.ConsultaAluno(escola);
+            break;
+
+         case 2:
+
+            break;
+
+         case 3:
+
+            break;
+
+         case 4:
+
+            break;
+
+         case 0:
+            Console.WriteLine("Voltando...");
+            Console.ReadKey();
+            break;
+
+         default:
+            Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            break;
+         }
+
+      }
+
+      static void Alteracoes(Escola escola) {
+         Console.Clear();
+         Console.WriteLine("Selecione uma opção de alteração:");
+         Console.WriteLine("1 - Vincular Aluno a turma");
+         Console.WriteLine("0 - Voltar");
+
+         int.TryParse(Console.ReadLine(), out int opcao);
+
+         switch (opcao) {
+         case 1:
+            escola.VincularAlunoATurma(escola);
+            break;
+
+         case 0:
+            Console.WriteLine("Voltando...");
+            Console.ReadKey();
+            break;
+
+         default:
+            Console.WriteLine("Opção inválida! Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+            break;
+         }
+      }
+   }
 }
