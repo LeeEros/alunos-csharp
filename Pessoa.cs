1a74e49ecd1b3c584328b76c72a62ba61da95d88
Gian Ilchechen <giancilchechen@gmail.com>
2023-07-05 00:22:46 -0300

    feat: implementado o metodo calcular idade e tornado abstrado o exibir informacoes

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,12 +1,29 @@
 using System;
 
-namespace Escola{
-    public abstract class Pessoa : IPessoa{
-        public string Nome{ get; set;}
-        public DateTime DataNascimento{ get; set;}
+namespace Escola {
+   public abstract class Pessoa: IPessoa {
+      public string Nome {
+         get;
+          set;
+      }
+      public DateTime DataNascimento {
+         get;
+          set;
+      }
 
-        public virtual void ExibirInformacoes(){
-            Console.WriteLine($"Nome: {Nome}\n Data de Nascimento: {DataNascimento.ToShortDateString()}");
-        } 
+      public int calculaIdade() {
+
+         DateTime hoje = DateTime.Now;
+
+         int idade = hoje.Year - DataNascimento.Year;
+
+         if (DataNascimento.Date > hoje.AddYears(-idade)) {
+            idade--;
+         }
+
+         return idade;
+      }
+
+      public abstract void ExibirInformacoes();
    }
 }
